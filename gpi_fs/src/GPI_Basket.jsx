63e659e3bf646a3046cd2945455913775d7d19ec
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2021-12-06 06:23:28 +0300

    main : Added func delete_basket

--- gpi_fs/src/GPI_Basket.jsx
+++ gpi_fs/src/GPI_Basket.jsx
@@ -1,46 +1,69 @@
+import { useEffect, useState } from "react";
 import gpi_download_file from "./gpi_download_file";
 
 export default function GPI_Basket() {
-    const gpi_Basket = gpi_get_busket();
+    const [gpi_Basket, gpi_setBasket] = useState({});
 
-    function gpi_get_busket() {
-        let gpi_str_busket;
-        let gpi_busket;
+    // Конструктор
+    useEffect(() => {
+        gpi_reload_basket();
+    }, []);
+
+    function gpi_reload_basket() {
+        gpi_setBasket(gpi_get_basket());
+    }
+
+    function gpi_get_basket() {
+        let gpi_str_basket;                                     // Объект в строке
+        let gpi_basket;                                         // Объект ключ - модель, значение - количество
         // = = = = = = = = = = = = = = = =
 
-        gpi_str_busket = sessionStorage.getItem("gpi_basket");
+        gpi_str_basket = sessionStorage.getItem("gpi_basket");  // Получаем объект в строке из БД
 
-        if (gpi_str_busket == null) {
-            console.log(gpi_Basket);
-            return {};
+        if (gpi_str_basket == null) {                           // Если в БД нет
+            return {};                                          // Возвращаем пустой объект
         }
 
-        gpi_busket = JSON.parse(gpi_str_busket);
+        gpi_basket = JSON.parse(gpi_str_basket);                // Преобразуем объект в строке в объект
 
-        if (Object.prototype.toString.call(gpi_busket) !== "[object Object]") {
-            return {};
+        // Если это не объект
+        if (Object.prototype.toString.call(gpi_basket) !== "[object Object]") {
+            return {};                                          // Возвращаем пустой объект
         }
 
-        return gpi_busket;
+        return gpi_basket;                                      // Возвращаем объект
     }
 
     function gpi_print() {
         gpi_download_csv();
     }
 
-    async function gpi_download_csv() {
-        let gpi_str = "";
+    function gpi_download_csv() {
+        let gpi_str = "";                                       // Текст файла
     
-        gpi_str += `"", "Товар", "Количество"\n`;
+        gpi_str += `"", "Товар", "Количество"\n`;               // Добавили CSV заголовок
 
         Object.keys(gpi_Basket).forEach(function (key, index) {
-            gpi_str += `"${index + 1}", "${key}", "${gpi_Basket[key]}"\n`;
+            gpi_str += `"${index + 1}", "${key}", "${gpi_Basket[key]}"\n`; // Добавили CSV тело
         })
       
-        const D = new Date();
+        const D = new Date();                                   // Получаем сегодняшнюю дату
         const FILE_NAME = `${D.getFullYear()}-${D.getMonth()}-${D.getDate()}_${D.getHours()}-${D.getMinutes()}_basket.csv`;
     
-        gpi_download_file(gpi_str, FILE_NAME);
+        gpi_download_file(gpi_str, FILE_NAME);                  // Вызываем функцию скачивания файла
+    }
+
+    function gpi_delete_basket(model) {
+        let gpi_str_basket;                                     // Объект в строке
+        let gpi_basket;                                         // Объект ключ - модель, значение - количество
+        // = = = = = = = = = = = = = = = =
+
+        gpi_basket = gpi_Basket;                                // Записываем объект
+        delete gpi_basket[model];                               // Удаляем из корзины модель
+        gpi_str_basket = JSON.stringify(gpi_basket);            // Преобразуем объект в строку
+        sessionStorage.setItem("gpi_basket", gpi_str_basket);   // Записываем в БД строку
+
+        gpi_reload_basket();                                    // Обновляем компонент
     }
 
     return (
@@ -62,11 +85,19 @@ export default function GPI_Basket() {
                 <tbody>
                     {
                         Object.keys(gpi_Basket).map(
-                            (key, index) => (
+                            (model, index) => (
                                 <tr key={index}>
                                     <th>{index + 1}</th>
-                                    <td>{key}</td>
-                                    <td>{gpi_Basket[key]}</td>
+                                    <td>{model}</td>
+                                    <td>{gpi_Basket[model]}</td>
+                                    <td>
+                                        <button
+                                            className="btn btn-outline-danger"
+                                            onClick={event => gpi_delete_basket(model)}
+                                        >
+                                            Убрать с корзины
+                                        </button>
+                                    </td>
                                 </tr>
                             )
                         )
