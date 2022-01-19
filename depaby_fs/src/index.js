de0277868ab41392169d56d6cf560aabff24877f
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-01-19 23:22:41 +0300

    main : Added hash and browser routers (module fs)

--- depaby_fs/src/index.js
+++ depaby_fs/src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter as Router} from 'react-router-dom';
-import App from './App';
+import { HashRouter, BrowserRouter } from 'react-router-dom';
 // import 'bootstrap/dist/css/bootstrap-grid.min.css';
+
+import App from './App';
 
 ReactDOM.render(
     (
         <React.StrictMode>
-            <Router>
-                <App />
-            </Router>
+            {
+                process.env.REACT_APP_HashRouter === 'true' ? (
+                    <HashRouter>
+                        <App />
+                    </HashRouter>
+                ) : (
+                    <BrowserRouter>
+                        <App />
+                    </BrowserRouter>
+                )
+            }
         </React.StrictMode>
     ),
     document.getElementById('root')
