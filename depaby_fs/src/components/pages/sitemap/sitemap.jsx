f56e00b55be776fbcf89a983b917105ae5b367c2
Pavel-Innokentevich-Galanin <Pavel.Innokentevich.Galanin@gmail.com>
2022-01-23 21:59:57 +0300

    main : Updated sitemap (module fs)

--- depaby_fs/src/components/pages/sitemap/sitemap.jsx
+++ depaby_fs/src/components/pages/sitemap/sitemap.jsx
@@ -40,15 +40,12 @@ function Sitemap(props) {
 
     function download_sitemap() {
         let text = "";
-        text += `
-<?xml version="1.0" encoding="UTF-8"?>
-
+        text += `<?xml version="1.0" encoding="UTF-8"?>
 <urlset
-  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-  xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-    http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
->
+    xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+        http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
 `;
 
         const date = new Date();
